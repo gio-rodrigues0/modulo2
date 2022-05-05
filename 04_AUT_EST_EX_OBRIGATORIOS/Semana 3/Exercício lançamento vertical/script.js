40741f059fe7df9ac77509fbd802e5ebafe7dcde
gio-rodrigues0 <99195612+gio-rodrigues0@users.noreply.github.com>
2022-05-05 14:24:01 -0300

    Adicionado exercício de lançamento vertical

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/Exercício lançamento vertical/script.js
@@ -0,0 +1,8 @@
+
+function calcule(){
+    var velocidade = document.getElementById("velocidadeInicial").value;
+    var altura = (velocidade**2)/20;
+    var tempo = velocidade/10;
+    document.getElementById("alturaMaxima").innerHTML = altura;
+    document.getElementById("tempoTotal").innerHTML = tempo;
+}
